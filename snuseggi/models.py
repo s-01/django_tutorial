371e0c3ce145fa36e5fc5b51fd388ef6a2dc8912
wjdtmdrms@gmail.com <wjdtmdrms@gmail.com>
2016-06-01 17:37:56 +0900

    everywhere

--- snuseggi/models.py
+++ snuseggi/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length = 50, primary_key = True)
-    point_0 = models.BooleanField()
-    point_1 = models.BooleanField()
-    point_2 = models.BooleanField()
-    point_3 = models.BooleanField()
-    point_4 = models.BooleanField()
+    rating = models.PositiveSmallIntegerField(default = 0)
+    point = models.PositiveSmallIntegerField(default = 0)
     comment = models.TextField()
--- snuseggi/views.py
+++ snuseggi/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Restaurant
 
 # Create your views here.
+def restaurants(request):
+    restaurant_list = Restaurant.objects.all().order_by('rating')
+    return render(request, 'snuseggi/restaurants.html', {'restaurant_list' : restaurant_list})
