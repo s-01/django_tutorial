9cf41db625c774429d2f6c5c007a54c39045356d
wjdtmdrms@gmail.com <wjdtmdrms@gmail.com>
2016-06-01 19:45:05 +0900

    db

--- snuseggi/admin.py
+++ snuseggi/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Restaurant
+from .models import Assessment
+from .models import Menu
 
 # Register your models here.
 admin.site.register(Restaurant)
+admin.site.register(Assessment)
+admin.site.register(Menu)
--- snuseggi/models.py
+++ snuseggi/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 
+class Menu(models.Model):
+    name = models.CharField(max_length = 50, primary_key = True)
+    
+    def __str__(self):
+        return self.name
+ 
 class Restaurant(models.Model):
     name = models.CharField(max_length = 50, primary_key = True)
-    rating = models.PositiveSmallIntegerField(default = 0)
     point_taste = models.PositiveSmallIntegerField(default = 0)
     point_service = models.PositiveSmallIntegerField(default = 0)
     point_price = models.PositiveSmallIntegerField(default = 0)
-    comment = models.TextField()
-    
+
     def point_avg(self):
         return (self.point_taste + self.point_service + self.point_price) / 3
+
+    def __str__(self):
+        return self.name
     
 class Assessment(models.Model):
+    CLASSIFICATION = (('L', 'Lunch'), ('D', 'Dinner'),)
     restuarant = models.ForeignKey('Restaurant')
+    classification = models.CharField(max_length=1, choices = CLASSIFICATION, default = 'L')
+    menus = models.ManyToManyField(Menu)
     date = models.DateField(auto_now = True)
     point_taste = models.PositiveSmallIntegerField(default = 0)
     point_service = models.PositiveSmallIntegerField(default = 0)
     point_price = models.PositiveSmallIntegerField(default = 0)
     comment = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        return self.name
